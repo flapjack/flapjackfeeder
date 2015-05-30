59f25941a97d33bd509564297c773785160518ba
Ali Graham <ali.graham@bulletproof.net>
2015-05-30 11:01:31 +0930

    parse module args, build redis address

--- src/flapjackruster.rs
+++ src/flapjackruster.rs
@@ -2,14 +2,16 @@
 
 extern crate libc;
 use libc::{c_char, c_int, c_ulong, c_void};
-use std::ffi::{CString};
+use std::ffi::{CString, CStr};
+use std::collections::{HashMap};
 
 extern crate redis;
+use redis::Commands;
 
 #[allow(non_camel_case_types, dead_code, non_snake_case, unused_variables, non_upper_case_globals)]
 mod nagios;
 
-// rust-bindgen doesn't seem to translate defined
+// rust-bindgen doesn't seem to translate defined constants
 const NEBMODULE_MODINFO_TITLE:     c_int = 0;
 const NEBMODULE_MODINFO_AUTHOR:    c_int = 1;
 const NEBMODULE_MODINFO_COPYRIGHT: c_int = 2;
@@ -34,36 +36,67 @@ pub extern "C" fn nebmodule_init(flags: c_int, args: *const c_char, handle: *mut
   let mut hand = &handle;
   let npcdmod_module_handle: *mut c_void = &mut hand as *mut _ as *mut c_void;
 
-  let title       = CString::new("flapjackruster").unwrap();
-  let author      = CString::new("Ali Graham").unwrap();
-  let copyright   = CString::new("(c) 2015 Ali Graham").unwrap();
-  let version     = CString::new("0.0.1").unwrap();
-  let license     = CString::new("GPLv2").unwrap();
-  let description = CString::new("A simple performance data / check result extractor / pipe writer.").unwrap();
+  let title       = CString::new("flapjackruster").unwrap().as_ptr() as *mut c_char;
+  let author      = CString::new("Ali Graham").unwrap().as_ptr() as *mut c_char;
+  let copyright   = CString::new("(c) 2015 Ali Graham").unwrap().as_ptr() as *mut c_char;
+  let version     = CString::new("0.0.1").unwrap().as_ptr() as *mut c_char;
+  let license     = CString::new("GPLv2").unwrap().as_ptr() as *mut c_char;
+  let description = CString::new("A simple performance data / check result extractor / pipe writer.").unwrap().as_ptr() as *mut c_char;
 
-  let announcement = CString::new("flapjackruster in the house!").unwrap();
+  let announcement = CString::new("flapjackruster in the house!").unwrap().as_ptr() as *mut c_char;
 
   unsafe {
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_TITLE,
-      title.as_ptr() as *mut c_char);
+      title);
 
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_AUTHOR,
-      author.as_ptr() as *mut c_char);
+      author);
 
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_COPYRIGHT,
-      copyright.as_ptr() as *mut c_char);
+      copyright);
 
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_VERSION,
-      version.as_ptr() as *mut c_char);
+      version);
 
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_LICENSE,
-      license.as_ptr() as *mut c_char);
+      license);
 
     nagios::neb_set_module_info(npcdmod_module_handle, NEBMODULE_MODINFO_DESC,
-      description.as_ptr() as *mut c_char);
+      description);
 
-    nagios::write_to_all_logs(announcement.as_ptr() as *mut c_char, NSLOG_INFO_MESSAGE);
+    nagios::write_to_all_logs(announcement, NSLOG_INFO_MESSAGE);
   }
+
+  // FIXME panics if args is empty
+  let kv_args = process_module_args( str_from_c_str(args) );
+
+  let mut redis_host = &"localhost";
+  let mut redis_port = &"6379";
+
+  if kv_args.contains_key(&"redis_host") {
+    redis_host = kv_args.get(&"redis_host").unwrap();
+  }
+
+  if kv_args.contains_key(&"redis_port") {
+    redis_port = kv_args.get(&"redis_port").unwrap();
+  }
+
+  let mut redis_address = String::new();
+  redis_address.push_str("redis://");
+  redis_address.push_str(redis_host);
+  redis_address.push_str(":");
+  redis_address.push_str(redis_port);
+
+  // let address_out = CString::new(redis_address.as_bytes()).unwrap().as_ptr() as *mut c_char;
+
+  // unsafe {
+  //   nagios::write_to_all_logs(address_out, NSLOG_INFO_MESSAGE);
+  // }
+
+  // let client = try!(redis::Client::open(redis_address));
+  // let con = try!(client.get_connection());
+
+  // let _ : () = try!(con.set("rustrustrust", 42));
 
   return 0;
 }
@@ -101,7 +134,25 @@ pub extern "C" fn npcdmod_handle_data(event_type: c_int, data: *mut c_void) -> c
   return 0;
 }
 
+fn str_from_c_str(s: *const c_char) -> &'static str {
+  let c_str: &CStr = unsafe { CStr::from_ptr(s) };
+  let buf: &[u8] = c_str.to_bytes();
+  return std::str::from_utf8(buf).unwrap();
+}
 
+// split line by commas, split by equals k=v
+fn process_module_args(args: &str) -> HashMap<&str, &str> {
+  let v: Vec<&str> = args.split(',').collect();
+
+  let mut kv_args = HashMap::new();
+
+  for a in v.iter() {
+    let av: Vec<&str> = a.splitn(2, '=').collect();
+    kv_args.insert(av[0], av[1]);
+  }
+
+  return kv_args;
+}
 
 // Ran into cyclic dependency errors trying to add the PPA to Puppet setup, thus
 // installing inside the VM:
