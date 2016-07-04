7f698e52b0eafea0799140a6ca1a51cfe601fdcd
Birger Schmidt <bs-github@netgaroo.com>
2016-07-04 22:28:32 +0200

    remove naemon header files (use naemon-dev package instead)

--- naemon/lib/libnagios.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBNAEMON_libnagios_h__
-#define LIBNAEMON_libnagios_h__
-/**
- * @file libnagios.h
- *
- * @brief Compat header to facilitate using Nagios projects with Naemon
- */
-#include "libnaemon.h"
-#endif /* LIBNAEMON_libnagios_h__ */
